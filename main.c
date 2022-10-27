2095de4900ad0ac6b875d9f726eede85ed4d8169
Asiluke <116753821+Asiluke@users.noreply.github.com>
2022-10-27 03:27:36 +0200

    ver 3
    can find using part of number only
    need to fix error with short lists

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include<string.h>
+#include <stdlib.h>
 
 
 
@@ -8,40 +9,34 @@
 int main(int argc, char *argv[] ) {
 
 
-
-
-
+//promene
     char a[126][101];
+    int counter = 0;
     char pomocna[100];
-    // int count = 1;
-    printf(" %d %s!\n",argc, argv[1]);
-    for(int b = 0; b < 126;b++){
-        scanf("%s",pomocna);
-        strcpy(a[b], pomocna );
+    int pom = strlen(argv[1]);
+
+    printf("  Zadany argument je %d %s!\n", argc, argv[1]);
+    system("cls");
+
+    //nacteni seznamu//
+    for (int b = 0; b < 126; b++) {
+        scanf("%s", pomocna);
+        strcpy(a[b], pomocna);
 
 
     }
+    //porovnani a vypsani//
+    for (int b = 0; b < 126; b++) {                 //vsechny jedontlive retezce
+        for (int g = 0; g < pom; g++) {             //pom je delka argumentu
+            if (a[b][g]== argv[1][g])counter++;}    //overuju jestli jednotli znaky v retezci jsou stejne jako v argumentu, pokud ano na pocitadle inkrementuju
+                if(counter==pom){printf("%s %s \n*%s \n", a[b - 1], a[b - 2], a[b]);
 
-    for(int b = 0; b < 126;b++){int porovnani;
-        //if (count > 3)count = 1;
 
-        porovnani =  strcmp(a[b],argv[1] );
-        if(porovnani==0)printf("Kontakt je %s %s \n%s ",a[b-1],a[b-2],a[b]);
-        //count++;
+                }
+                                                                          //pokud counter dostahl plnych bodu tak jsou vsecny znaky v retezci
+                                                                                                    // a argumentu stejne, vypisuju jeste dalsi dva pribuzne retezce
+        counter = 0;
+        }
+
+        return 0;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    return 0;
-}
